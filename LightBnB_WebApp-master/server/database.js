927a8a9b5faa063cc07f8bc72e6a99f20cf40b63
Sanjay Swamy <swamysanjay98@gmail.com>
2021-10-15 02:36:20 +0000

    Updated reservations functions

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -72,7 +72,16 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(` SELECT properties.* FROM reservations JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $2
+  AND reservations.end_date < now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $1`, [limit, guest_id])
+  .then((result) => result.rows)
+  .catch((err) => {console.log(err.message);
+  });
 }
 exports.getAllReservations = getAllReservations;
 
